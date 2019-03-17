a7b83b656750688208640756517560530d7442bc
ChiuYiKai <t106590008@ntut.org.tw>
2019-03-17 17:08:58 +0800

    basicObject

--- game4.10/Source/BasicObject.cpp
+++ game4.10/Source/BasicObject.cpp
@@ -7,27 +7,57 @@
 #include "gamelib.h"
 
 namespace game_framework {
-	void BasicObject::OnMove()
-	{
-
-	}
-	void BasicObject::OnShow()
-	{
-	}
-	void BasicObject::SetXY()
-	{
-	}
-	bool BasicObject::isAlive()
-	{
-		return false;
-	}
-	bool BasicObject::isThourghable()
-	{
-		return false;
-	}
-	bool BasicObject::isBreakable()
-	{
-		return false;
-	}
-
+void BasicObject::OnMove()
+{
+	if (!(can_move && is_alive))
+		return;
+	if (isMovingLeft)
+		x -= speed;
+	if (isMovingRight)
+		x += speed;
+	if (isMovingUp)
+		y -= speed;
+	if (isMovingDown)
+		y += speed;
+void BasicObject::OnShow()
+{
+    if (is_alive)
+    {
+        bmp.SetTopLeft(x + dx, y + dy);
+        bmp.ShowBitmap();
+    }
+}
+void BasicObject::SetXY(int nx, int ny)
+{
+    x = nx;
+    y = ny;
+}
+void BasicObject::SetMovingUP(bool flag)
+{
+	isMovingUp = flag;
+}
+void BasicObject::SetMovingDown(bool flag)
+{
+	isMovingDown = flag;
+}
+void BasicObject::SetMovingLeft(bool flag)
+{
+	isMovingLeft = flag;
+}
+void BasicObject::SetMovingRight(bool flag)
+{
+	isMovingRight = flag;
+}
+bool BasicObject::isAlive()
+{
+    return is_alive;
+}
+bool BasicObject::isThourghable()
+{
+    return can_thourghable;
+}
+bool BasicObject::isBreakable()
+{
+    return can_breakable;
+}
 }
--- game4.10/Source/BasicObject.h
+++ game4.10/Source/BasicObject.h
@@ -5,14 +5,25 @@ namespace game_framework {
 	public:
 		void OnMove();
 		void OnShow();
-		void SetXY();
+		void SetXY(int nx, int ny);
+		void SetMovingUP(bool flag);
+		void SetMovingDown(bool flag);
+		void SetMovingLeft(bool flag);
+		void SetMovingRight(bool flag);
+
 		bool isAlive();
 		bool isThourghable();
 		bool isBreakable();
 		virtual void LoadBitMap() = 0;
 	protected:
+		CMovingBitmap bmp;		
 		int speed;
 		int HP;
 		int size;
+		int x, y, dx, dy;
+		bool can_breakable, can_thourghable, is_alive, can_move;
+		bool isMovingDown, isMovingLeft, isMovingRight, isMovingUp;
+
+
 	};
 }
